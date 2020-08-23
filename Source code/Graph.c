6304695338f345f38102b2c0b358645b56f6f2fd
Claudio Tancredi <Claudiotancredi98@gmail.com>
2020-08-23 08:05:45 +0200

    Deleted some comments

--- Source code/Graph.c
+++ Source code/Graph.c
@@ -12,7 +12,7 @@ static void  insertE(Graph G, Edge e);
 static void  removeE(Graph G, Edge e);
 static void dfsRcc(Graph G, int v, int id, int *cc);
 
-//funzioni interne non standard, prototipo aggiunto a casa
+//funzioni interne non standard
 int comb_sempl(int pos, Edge *val, Edge *vett, int n, int k, int start, int ok, Graph G);
 static int *VETTint(int v, int val);
 
@@ -49,7 +49,7 @@ static int **MATRIXint(int r, int c, int val) {
   return t;
 }
 
-static int *VETTint(int v, int val){ //funzione implementata a casa
+static int *VETTint(int v, int val){
     int i;
     int *vett;
     vett=malloc(v*sizeof(int));
@@ -68,8 +68,8 @@ Graph GRAPHinit(int V) {
   G->E = 0;
   G->madj = MATRIXint(V, V, 0);
 
-  G->vertex=VETTint(V, 1); //aggiunto a casa
-  G->nV=V;                 //aggiunto a casa
+  G->vertex=VETTint(V, 1); 
+  G->nV=V;                
 
   G->st = STinit(V);
   return G;
@@ -81,7 +81,7 @@ void GRAPHfree(Graph G) {
     free(G->madj[i]);
   free(G->madj);
 
-  free(G->vertex); //aggiunto a casa
+  free(G->vertex); 
 
   STfree(G->st);
   free(G);
@@ -149,16 +149,15 @@ void jedgeconnected(Graph G, int j){
     if (val==NULL)
         exit(1);
     GRAPHedges(G, val);
-    for (i=1; i<=G->E && cc==1; i++){  //dovevo controllare che ALMENO j archi lo sconnettessero
-            //ho cambiato il valore di fine ciclo
+    for (i=1; i<=G->E && cc==1; i++){ 
         vett=(Edge*)malloc(i*sizeof(Edge));
         if (vett==NULL)
             exit(1);
-        cc=comb_sempl(0, val, vett, G->E, i, 0, ok, G); //ho riordinato un po' i parametri
+        cc=comb_sempl(0, val, vett, G->E, i, 0, ok, G);
         if (cc==0 && i<j)
             printf("Esiste un insieme di archi di cardinalità < j che sconnette il grafo. "
                    "Grafo non j-edge-connected.\n");
-        else if (cc==0 && i>=j){ //ho cambiato la condizione, i>=j
+        else if (cc==0 && i>=j){
             printf("Grafo j-edge-connected. Archi che lo sconnettono:\n");
             for (k=0; k<i; k++){
                 printf("(%s, %s)\n", STsearchByIndex(G->st, vett[k].v), STsearchByIndex(G->st, vett[k].w));
@@ -166,12 +165,11 @@ void jedgeconnected(Graph G, int j){
         }
         free(vett);
     }
-    free(val); //aggiunto a casa
+    free(val);
     return;
 }
 
 int comb_sempl(int pos, Edge *val, Edge *vett, int n, int k, int start, int ok, Graph G){
-    //ho riordinato i parametri passati alla funzione
     int i, cc;
     if (pos>=k){
         for(i=0; i<k; i++){
@@ -205,7 +203,6 @@ int GRAPHcc(Graph G) {
   for (v = 0; v < G->V; v++)
     if (cc[v] == -1)
       dfsRcc(G, v, id++, cc);
-      //ho tolto la stampa che non mi serve
   return id;
 }
 
@@ -221,13 +218,8 @@ static void dfsRcc(Graph G, int v, int id, int *cc) {
 }
 
 void kcore(Graph G, int k, int *ok){
-    //nel compito ho dimenticato l'asterisco ad ok ma era chiaramente passato by reference
     int i, j, degree;
-    int *v=calloc(G->V,sizeof(int)); //ho aggiunto questo vettore per segnarmi gli archi
-    //che tolgo, che poi devo riaggiungerli
-    //posso inizializzarlo all'inizio (e non ad ogni iterazione)
-    //perché lo uso una sola volta per poi ricorrere,
-    //non lo riutilizzo più in una certa istanza della funzione
+    int *v=calloc(G->V,sizeof(int));
     if (v==NULL)
         exit(1);
     for (i=0; i<G->V; i++){
@@ -243,28 +235,26 @@ void kcore(Graph G, int k, int *ok){
                 for (j=0; j<G->V; j++){
                     if (G->madj[i][j]==1){
                         GRAPHremoveE(G, i, j);
-                        v[j]=1; //segno che ho tolto l'arco con j come estremo destro, aggiunto a casa
+                        v[j]=1;
                     }
                 }
                 G->nV--;
                 kcore(G, k, ok);
                 if (*ok==1){
-                    //in fase di chiusura ricorsiva devo ripristinare le condizioni iniziali del grafo!
-                    G->vertex[i]=1; //resetto il vertice
-                    G->nV++; //incremento il parametro
+                    G->vertex[i]=1;
+                    G->nV++;
                     for (j=0; j<G->V; j++){
-                        if (v[j]==1){ //se l'arco (i, j) era stato rimosso
-                            GRAPHinsertE(G, i, j); //lo reinserisco
+                        if (v[j]==1){
+                            GRAPHinsertE(G, i, j);
                         }
                     }
-                    free(v); //libero il vettore prima di fare return
+                    free(v);
                     return;
                 }
             }
         }
     }
     *ok=1;
-    //le stampe le ho aggiunte a casa
     if (G->nV==0){
         printf("Non ci sono vertici che formano il k-core!\n");
     }
@@ -276,6 +266,6 @@ void kcore(Graph G, int k, int *ok){
             }
         }
     }
-    free(v); //libero anche qui il vettore per evitare memory leak
+    free(v);
     return;
 }
--- Source code/main.c
+++ Source code/main.c
@@ -11,17 +11,16 @@ int main(int argc, char *argv[]){
     int j, ok=0;
     G=GRAPHload(fp);
     fclose(fp);
-    //ho riordinato il main chiamando prima la funzione per il punto 2 e poi quella per il punto 3
     printf("Inserire un intero k maggiore o uguale a 0.\n");
     scanf("%d", &j);
     if (j<0)
-        exit(1); //controllo aggiunto a casa
+        exit(1);
     kcore(G, j, &ok);
     printf("Inserire un intero j maggiore o uguale a 1.\n");
     scanf("%d", &j);
     if (j<1)
-        exit(1); //controllo aggiunto a casa
+        exit(1);
     jedgeconnected(G, j);
-    GRAPHfree(G); //aggiunta a casa
+    GRAPHfree(G);
     return 0;
 }
